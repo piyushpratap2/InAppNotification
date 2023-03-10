a6b00090dbfebb2619f08b68bf7ac903aa859a2a
Piyush <piyush.pratap@sunfox.in>
2023-03-10 16:48:51 +0530

    InAppNotification

---
+++ app/src/main/java/com/example/inappnotification/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.inappnotification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ app/src/main/java/com/example/inappnotification/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.inappnotification
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
